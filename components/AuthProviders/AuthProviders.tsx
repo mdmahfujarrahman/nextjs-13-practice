c26d270213f91a4b90ddc15b3df9561092891737
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-07-29 09:01:07 +0600

    check

--- components/AuthProviders/AuthProviders.tsx
+++ components/AuthProviders/AuthProviders.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { type } from "os";
+import { useState, useEffect } from "react";
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-    return <div>AuthProviders</div>;
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const providers = await getProviders();
+            console.log(providers);
+            setProviders(providers);
+        };
+        fetchProviders();
+    }, []);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider) => (
+                    <button key={provider.id}>{provider.id}</button>
+                ))}
+            </div>
+        );
+    }
 };
 
 export default AuthProviders;
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { NavbarLinks } from "../constant";
 import AuthProviders from "../AuthProviders/AuthProviders";
 
 const Navbar = () => {
-    const session = {};
+    const session = null;
     return (
         <nav className="flexBetween navbar">
             <div className="flex-1 flexStart gap-10">
